225b6508d45f8c87f5717ed8396d3b4822b38df5
Nick Sandonato <nick.sandonato@gmail.com>
2024-01-31 11:31:58 -0500

    Add more gitsigns bindings

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,12 +1,17 @@
 return {
   {
-    'lewis6991/gitsigns.nvim',
-    event = 'BufEnter',
+    "lewis6991/gitsigns.nvim",
+    event = "BufEnter",
     config = function()
-      local gitsigns = require('gitsigns')
-      gitsigns.setup()
-      vim.keymap.set('n', '<leader>gtb', gitsigns.toggle_current_line_blame, { desc = 'Toggle current line blame' })
-      vim.keymap.set('n', '<leader>gb', function() gitsigns.blame_line{full=true} end, { desc = 'Show blame' })
+      local gitsigns = require("gitsigns")
+      gitsigns.setup({
+        current_line_blame_opts = {
+          delay = 200
+        }
+      })
+      vim.keymap.set("n", "<leader>gtb", gitsigns.toggle_current_line_blame, { desc = "Toggle current line blame" })
+      vim.keymap.set("n", "<leader>gb", function() gitsigns.blame_line{full=true} end, { desc = "Show blame" })
+      vim.keymap.set("n", "<leader>gph", gitsigns.preview_hunk, { desc = "Preview the hunk" })
     end
   }
 }
